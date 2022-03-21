e81f05ed67693f5faf571b1adfb2a07e73d828b8
Kota Kanazawa <kota0818@gmail.com>
2022-03-21 16:32:07 +0900

    Initial commit

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { FeedbackStore } from '../stores'
+  import { fade, scale } from 'svelte/transition'
+  import FeedbackItem from './FeedbackItem.svelte'
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+  <div in:scale out:fade="{{ duration: 300 }}">
+    <FeedbackItem item={fb} />
+  </div>
+{/each}
